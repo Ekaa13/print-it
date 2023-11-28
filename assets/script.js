9af281850717b71802351957c4376253fe9a75c8
Alexis Rebeyrol <aldorigen28@gmail.com>
2023-11-28 12:02:58 +0100

    js add

--- assets/script.js
+++ assets/script.js
@@ -15,4 +15,60 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
+const bannerImg = document.querySelector('.banner-img');
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const dots = document.querySelectorAll('.dot');  
+
+let currentIndex = 0;
+
+function updateDots(index) {
+    dots.forEach((dot, i) => {
+        if (i === index) {
+            dot.classList.add('dot_selected');
+        } else {
+            dot.classList.remove('dot_selected'); 
+        }
+    });
+}
+
+function updateCarousel(index, direction) {
+  //infinite
+	if (currentIndex === -1 && direction === 'left') {
+	  currentIndex = slides.length - 1;
+  } else if (currentIndex === slides.length && direction === 'right') {
+	  currentIndex = 0;
+  }
+
+  //maj img
+  const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
+  bannerImg.src = imagePath;
+  bannerImg.alt = `Slide ${currentIndex + 1}`;
+
+  //maj txt
+  const tagLine = slides[currentIndex].tagLine;
+  document.querySelector('p').innerHTML = tagLine;
+
+  console.log(`Clic sur la flèche ${direction}`);
+}
+
+// event left arrow
+arrowLeft.addEventListener('click', function () {
+  currentIndex = (currentIndex - 1);
+  updateCarousel(currentIndex, 'left');
+  updateDots(currentIndex); 
+});
+
+// event right arrow
+arrowRight.addEventListener('click', function () {
+  currentIndex = (currentIndex + 1) ;
+  updateCarousel(currentIndex, 'right');
+  updateDots(currentIndex); 
+});
+
+
+// first img when reloading
+updateCarousel(currentIndex, 'démarrage');
+updateDots(currentIndex); 
